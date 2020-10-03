c9c7f821c9ae75bdee72a7f9b7bb5ae833ea0c93
Ubuntu <ubuntu@ip-172-31-33-116.us-east-2.compute.internal>
2020-10-03 15:49:10 +0000

    basic framework in place: routes and models

--- forum/backend/routes/boards.js
+++ forum/backend/routes/boards.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 let Board = require('../models/board.model');
 
+//default - get list of all boards
 router.route('/').get((req, res) => {
   Board.find()
     .then(board => res.json(board))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//add board
 router.route('/add').post((req, res) => {
   const board_title_text = req.body.board_title_text;
   const board_desc_text = req.body.board_desc_text;
@@ -15,6 +17,22 @@ router.route('/add').post((req, res) => {
     board_desc_text
   });
 
+//find board by ID
+
+  router.route('/:id').get((req, res) => {
+  Board.findById(req.params.id)
+    .then(board => res.json(board))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//delete board
+router.route('/:id').delete((req, res) => {
+  Board.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Board deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//save new board
   newBoard.save()
     .then(() => res.json('Board added!'))
     .catch(err => res.status(400).json('Error: ' + err));
