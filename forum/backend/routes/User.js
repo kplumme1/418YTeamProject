084510f543fcd27f2f138aa3d240943ab44756d4
James Catuccio <Jamesmail403@yahoo.com>
2020-10-20 03:21:05 +0000

    updated user schema and registration

--- forum/backend/routes/User.js
+++ forum/backend/routes/User.js
@@ -14,14 +14,21 @@ const signToken = userID => {
 }
 
 userRouter.post('/register', (req,res)=>{
-    const { username,password,role } = req.body;
+    const { username,email,password,role } = req.body;
+    User.findOne({email: req.body.email.toLowerCase()}, function (err) {
+	if(err) {
+	    return res.status(500).send("An unexpected error occured.");
+		}
+});
+	if(user)
+	    return res.status(400).send({message: "Email is already taken, try logging in"});
     User.findOne({username},(err,user)=>{
         if(err)
             res.status(500).json({message: {msgBody : "Error has occured", msgError : true}})
         if(user)
             res.status(400).json({message: {msgBody : "Username is already taken.", msgError : true}})
         else{
-            const newUser = new User({username,password,role});
+            const newUser = new User({username,email,password,role});
             newUser.save(err=>{
                 if(err)
                     res.status(500).json({message: {msgBody : "Error has occured", msgError : true}})
