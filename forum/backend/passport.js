d3269cc4dd31169cf3323fc1c57cb077a7969251
Shawnyg <shawnyg@no.com>
2020-11-16 17:24:58 -0500

    no compile errors
    cleared of compile errors

--- forum/backend/passport.js
+++ forum/backend/passport.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const User = require('../models/User');
-const keys = require("../config/keys");
+const User = require("./models/User");
+const keys = require("./config/keys");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
--- forum/backend/validation/login.js
+++ forum/backend/validation/login.js
@@ -1,11 +1,10 @@
 const Validator = require("validator");
-const isEmpty = require("is-empty");
 
 module.exports = function validateLoginInput(data) {
     let errors = {};
 // Convert empty fields to an empty string so we can use validator functions
-    data.email = !isEmpty(data.email) ? data.email : "";
-    data.password = !isEmpty(data.password) ? data.password : "";
+    data.email = !Validator.isEmpty(data.email) ? data.email : "";
+    data.password = !Validator.isEmpty(data.password) ? data.password : "";
 // Email checks
     if (Validator.isEmpty(data.email)) {
         errors.email = "Email field is required";
@@ -18,6 +17,6 @@ module.exports = function validateLoginInput(data) {
     }
     return {
         errors,
-        isValid: isEmpty(errors)
+        isValid: Validator.isEmpty(errors)
     };
 };
