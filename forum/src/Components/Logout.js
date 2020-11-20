19742d6e3b359326a52ec2a711e637d1e905abad
Shawnyg <shawnyg@no.com>
2020-11-19 23:12:43 -0500

    removed useless imports

--- forum/src/Components/Logout.js
+++ forum/src/Components/Logout.js
@@ -1,16 +1,3 @@
-
-import React from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Image from 'react-bootstrap/Image'
-import profpic from '../profpic.png'
-import axios from 'axios';
-
-import {Component} from "react";
-
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
