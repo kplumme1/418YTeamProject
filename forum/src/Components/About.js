cc57152c553a182f16217d80ee194a0b67966223
P Atrey <patrey@Ps-MacBook-Pro.local>
2020-11-22 00:22:33 -0500

    Added the About and Help pages

--- forum/src/Components/About.js
+++ forum/src/Components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
 var config = require('../config.json');
 
 function About() {
@@ -6,8 +7,14 @@ function About() {
         <div style = {{textAlign: "center"}}>
             <h1 style = {{textDecoration: "underline"}}>ABOUT</h1>
             <h3 style = {{margin: "20px 20%"}}>{config.aboutPage}</h3>
+            <Image src = {config.aboutPhoto} height = "300"></Image>
+            <hr></hr>
+            <h3 style = {{margin: "20px 20%"}}>The forum provides the users with the ability to create topics and posts. 
+                Users can reply to each other's posts and communicate. In order to use 4UM 
+                to the best extent, you can register for an account!
+                 </h3>
         </div>
     );
 }
 
-export default About
+export default About;
